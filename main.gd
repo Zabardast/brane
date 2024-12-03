072b641a5b4d0d6c4e2e06adf3e0160496c2b4bd
loup <hugo.molle@yahoo.fr>
2024-12-03 08:38:02 -0800

    simplify signal managment to send game conf to main instance

--- main.gd
+++ main.gd
@@ -8,9 +8,9 @@ func _ready() -> void:
 	var menu = $Menu
 	print_debug("Menu class:", menu.get_class())
 	menu.connect("start_game", Callable(self, "_start_game"))
-	menu.connect("menu_set_number_size", Callable(self, "menu_set_number_size"))
-	menu.connect("menu_set_list_size", Callable(self, "menu_set_list_size"))
-	menu.connect("menu_set_complexity", Callable(self, "menu_set_complexity"))
+	menu.connect("menu_set_number_size", Callable(self, "_menu_set_number_size"))
+	menu.connect("menu_set_list_size", Callable(self, "_menu_set_list_size"))
+	menu.connect("menu_set_complexity", Callable(self, "_menu_set_complexity"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -24,14 +24,14 @@ func _start_game() -> void:
 	print("complexity: ", complexity)
 	#pass
 
-func menu_set_number_size(p_val: int) -> void:
+func _menu_set_number_size(p_val: int) -> void:
 	print("number size ", p_val)
 	number_size = p_val
 	
-func menu_set_list_size(p_val: int) -> void:
+func _menu_set_list_size(p_val: int) -> void:
 	print("list size ", p_val)
 	list_size = p_val
 
-func menu_set_complexity(p_val: int) -> void:
+func _menu_set_complexity(p_val: int) -> void:
 	print("complexity ", p_val)
 	complexity = p_val
